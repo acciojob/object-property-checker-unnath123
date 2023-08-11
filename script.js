9ce4152215550f15056e925ec2eb557981408666
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-08-11 23:43:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	if(sampleObject.hasOwnProperty(key)){
+	if(key in sampleObject){
 		return true;
 	}
 	else
 		return false;
-	
-}
+	}
 
 // Do not change the code below
 const key = prompt("Enter Key.");
